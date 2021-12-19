fbea53803f825b82b1cd866c0afcacf30ab3b014
uma-kamuni <kamuniumaramesh@gmail.com>
2021-12-19 14:50:46 -0800

    Modified: Calculate Daily Employee Wage

--- src/com/bridgelabz/employeewage/EmployeeWage.java
+++ src/com/bridgelabz/employeewage/EmployeeWage.java
@@ -5,12 +5,16 @@ public class EmployeeWage {
 	public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program Using OOPS");
         int IS_FULL_TIME = 1;
+		int EMP_RATE_PER_HOUR = 20;
+		int empHrs = 0;
+		int empWage = 0;
 		double empCheck = Math.floor(Math.random()*10)%2;
 		if (empCheck == IS_FULL_TIME)
-		System.out.println("Employee is present");
+			empHrs = 8;
 		else
-			System.out.println("Employee is Abscent");
-
+			empHrs = 0;
+		empWage = empHrs*EMP_RATE_PER_HOUR;
+		System.out.println("Emp Wage:" + empWage);
 	}
 
 }
