756c3a56fc9c556811ca7ccd9310bdfc33324cac
uma-kamuni <kamuniumaramesh@gmail.com>
2021-12-19 14:44:52 -0800

    Added: Check Employee is Present or Absent

--- src/com/bridgelabz/employeewage/EmployeeWage.java
+++ src/com/bridgelabz/employeewage/EmployeeWage.java
@@ -4,6 +4,12 @@ public class EmployeeWage {
 
 	public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program Using OOPS");
+        int IS_FULL_TIME = 1;
+		double empCheck = Math.floor(Math.random()*10)%2;
+		if (empCheck == IS_FULL_TIME)
+		System.out.println("Employee is present");
+		else
+			System.out.println("Employee is Abscent");
 
 	}
 
